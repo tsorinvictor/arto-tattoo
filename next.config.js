0fc598abe7d26548b256590ea088b0dfa00cc5ed
Victor T. <tsorinvictor@gmail.com>
2026-02-20 11:39:06 +0200

    feat: Add global SCSS styles, configure Next.js base path and asset prefix, and introduce gallery image data.

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ const nextConfig = {
     includePaths: [path.join(__dirname, "src/styles")],
     prependData: `@import "@styles/variables"; @import "@styles/mixins";`,
   },
-  basePath: "",
-  assetPrefix: "",
+  basePath: "/arto-tattoo",
+  assetPrefix: "/arto-tattoo",
   output: "export",
   trailingSlash: true,
   images: {
